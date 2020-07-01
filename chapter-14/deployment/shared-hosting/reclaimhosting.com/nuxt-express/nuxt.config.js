7439f16b66f8666bfc0bd271dbee10fca07c18d3
Lau Tiam kok <lau.tiamkok@yahoo.co.uk>
2020-07-01 21:51:11 +0200

    Updated code for chapter 14

--- chapter-14/deployment/shared-hosting/reclaimhosting.com/nuxt-express/nuxt.config.js
+++ chapter-14/deployment/shared-hosting/reclaimhosting.com/nuxt-express/nuxt.config.js
@@ -1,8 +1,20 @@
 
 module.exports = {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
   mode: 'universal',
+
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
+
   /*
   ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
     title: process.env.npm_package_name || '',
@@ -15,34 +27,47 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+
   server: {
     port: 80, // default: 3000
     host: 'nuxt.stateu.org' // default: localhost
   },
+
   /*
   ** Customize the progress-bar color
   */
   loading: { color: '#fff' },
+
   /*
   ** Global CSS
   */
   css: [
   ],
+
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
   ],
+
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+
   /*
   ** Nuxt.js dev-modules
   */
   buildModules: [
   ],
+
   /*
   ** Nuxt.js modules
   */
   modules: [
   ],
+
   /*
   ** Build configuration
   */
